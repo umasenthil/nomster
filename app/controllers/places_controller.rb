978dde520f15793a47b1e448e32014b8ba14cf4c
uma senthil <uma_senthil@hotmail.com>
2015-10-18 18:13:21 +0000

    same as above for Delete feature

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, :only => [:new, :create, :edit]
+	before_action :authenticate_user!, :only => [:new, :create, :edit, :destroy]
 	def index
 		@places = Place.page(params[:page]).order(:name)
 	end
@@ -36,6 +36,10 @@ class PlacesController < ApplicationController
 	
 	def destroy
 		@place = Place.find(params[:id])
+		
+		if @place.user != current_user
+			return render :text => 'Not Allowed', :status => :forbidden
+		end
 		@place.destroy
 		redirect_to root_path
 	end
