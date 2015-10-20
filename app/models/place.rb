872299f86150ca49db5377e124b18ff3356ebbc6
uma senthil <uma_senthil@hotmail.com>
2015-10-20 06:53:46 +0000

    More validation for address, description and lenght for name

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,5 +3,9 @@ class Place < ActiveRecord::Base
 	
 	belongs_to :user
 	
-	validates :name, :presence => true
+	validates :name, length: { minimum: 3 }
+	
+	validates :address, :presence => true
+	
+	validates :description, :presence => true
 end
