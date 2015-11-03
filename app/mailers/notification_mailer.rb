bff989c95504925ba3fe94a0257fa4d5b7d6e4c8
uma senthil <uma_senthil@hotmail.com>
2015-11-03 16:22:21 +0000

    Added url link to the email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
   
-  def comment_added
-	mail(to: "umamaheswarinr@gmail.com", subject: "A commen has been added to your place")
+  def comment_added(comment)
+	@place = comment.place
+	@place_owner = @place.user
+	
+	mail(to: @place_owner.email, subject: "A commen has been added to your #{@place.name}")
   end
 end
