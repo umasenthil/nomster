19de1dc73b822d029969b04b21c583058680b8a4
uma senthil <uma_senthil@hotmail.com>
2015-11-03 17:11:43 +0000

    Added MailGun add to support email in heroku

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ActiveRecord::Base
 
 	belongs_to :user
 	belongs_to :place
+	after_create :send_comment_email
 	
 	RATINGS = {
 		'one star'    => '1_star',
@@ -14,4 +15,8 @@ class Comment < ActiveRecord::Base
 	def humanized_rating
 		RATINGS.invert[self.rating]
 	end
+	
+	def send_comment_email
+		NotificationMailer.comment_added(self).deliver
+	
 end
